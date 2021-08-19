d22c16acfebf274b0fe608349d6ae3fc14ba3302
mike@mikestratton.net <mike@mikestratton.net>
2021-08-19 19:57:15 -0400

    landing page complete

--- index.php
+++ index.php
@@ -1,1 +1,2 @@
 <?php
+include 'views/layouts/landing.php';
--- views/layouts/landing.php
+++ views/layouts/landing.php
@@ -1,9 +1,44 @@
 <!doctype html>
 <html>
 <head>
-    <?php include '../partials/head.php'; ?>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="daomega.com is a crypto & block chain start up.
+                                    We offer freelance software development, consultations and training">
+    <meta name="author" content="Michael A. Stratton">
+
+    <title>DAO mega - Crypto & blockchain development, consultations and training</title>
+
+    <link rel="canonical" href="http://daomega.com">
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+
+    <style>
+       body{
+           background-color: #002D5A;
+           color: #C0C0C0;
+       }
+
+    </style>
 </head>
 <body>
+<div class="container-fluid">
+    <div class="px-4 pt-5 my-5 text-center">
+        <div class="col-lg-6 mx-auto">
+            <h2>DAOMEGA is a startup company soon to offer crypto & blockchain development, consultations & training. </h2>
 
+        </div>
+        <div style="max-height: 50vh;">
+            <div class="container px-5 mt-5">
+                <div class="row justify-content-md-center">
+                    <div class="col-3">
+                        <img src="images/logo-large.png" class="img-fluid mb-4" alt="DAOmega logo" loading="lazy">
+
+                    </div>
+                </div>
+        </div>
+    </div>
+</div>
 </body>
 </html>
