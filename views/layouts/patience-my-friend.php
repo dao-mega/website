7ee1423962b575c0dc9fd0a3b0c66fc186d783ca
mike@mikestratton.net <mike@mikestratton.net>
2021-09-21 15:14:35 -0400

    links

--- views/layouts/patience-my-friend.php
+++ views/layouts/patience-my-friend.php
@@ -68,7 +68,7 @@
     <div class="row mt-5 mb-3">
         <div class="col-5"></div>
         <div class="col-2">
-            <p class="text-center"><a href="/website"><img height="150px" width="auto"  src="images/logo-v1-icon.png" alt="Logo Version 1 Warriors"></a></p>
+            <p class="text-center"><a href="index.php"><img height="150px" width="auto"  src="images/logo-v1-icon.png" alt="Logo Version 1 Warriors"></a></p>
         </div>
         <div class="col-5"></div>
 
