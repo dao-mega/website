ea701a0e77df4945eeac66fe404b95a0c9143256
mike@mikestratton.net <mike@mikestratton.net>
2021-10-25 22:28:12 -0400

    laravel

---
+++ tailwind/react-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './daomega-v2.2.svg';
+import './App.css';
+
+function App() {
+  return (
+
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+
+    </div>
+  );
+}
+// reload
+export default App;
---
+++ views/layouts/rotate.php
@@ -0,0 +1,207 @@
+<?php
+    $colors = ["#0f5132", "#002D5A", "#842029", "#212529", "#673ab7", "#cf3200", "#008ea0", "#607d8b",
+               "#292929", "#007cb5", "#ee7752", "#e73c7e", "#23a6d5", "#23d5ab"];
+    $rand_col = random_int(0, 13);
+
+    $t1 = "automated algorithms that drastically increase your crypto investment";
+    $t2 = "token access to people in poverty without internet";
+    $t3 = "Mega Seed and Mega Sprout tokens are under development";
+    $t4 = "short term investments without concern for loss";
+    $t5 = "smart contract updates based on semantic versioning";
+    $t6 = "daomega target profit: $1,000,000 by end of 2024";
+    $t7 = "improve investors control over market value";
+    $t8 = "short term traders can define worse case scenario";
+    $t9 = "community driven updates to smart contract rules";
+    $t10 = "worlds first physical crypto currency";
+    $t11 = "investors bids establish the value of the Mega Seed Token";
+    $t12 = "equity based tokens";
+    $t13 = "for as little as $1, you can purchase a token";
+    $t14 = "Mega Smart Tree smart contract is under development";
+    $title = [$t1, $t2, $t3, $t4, $t5, $t6, $t8, $t9, $t10, $t11, $t12, $t13, $t14];
+    $rand_ttl = random_int(0,13);
+
+    $l1 = "logo-v2-1.png";
+    $l2 = "logo-v2-blockchain1.png";
+    $l3 = "logo-v2-blockchain5.png";
+    $l4 = "logo-v2-blockchain3.png";
+    $l5 = "logo-v2-blockchain4.png";
+//    $l6 = "gold-dragon.png";
+    $l7 = "mega-smart-tree.png";
+    /*$l8 = "logo-v2.png";*/
+    $logo = [$l1,$l3,$l2,$l4,
+             $l5,$l7];
+    $rand_log = random_int(0,5);
+
+    $rand2 = random_int(1, 9);
+    $rand4 = random_int(3,8);
+
+//    $rand = array('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f');
+    $color = 'rgba(' . random_int(0,255) . ',' . random_int(0,255) . ',' . random_int(0,255) . ',' . '0.3' . ')';
+    $bg = $color;
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="we create mathematically validated crypto investment algorithms">
+    <meta name="author" content="Michael A. Stratton">
+    <!--<meta http-equiv="refresh" content="15; URL=https://daomega.com">-->
+
+    <title>crypto startup company</title>
+
+    <link rel="canonical" href="http://daomega.com">
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+
+    <!-- vanta.js birds -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r121/three.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/vanta@latest/dist/vanta.birds.min.js"></script>
+
+
+    <link rel="stylesheet" href="style/cash.css">
+    <style>
+        .silver {
+            background-color: <?php echo $bg; ?>;
+            border-top: #fff thin solid;
+            border-bottom: #fff thin solid;
+        }
+
+        @-webkit-keyframes rotate {
+            from {
+                -webkit-transform: rotate(0deg);
+            }
+            to {
+                -webkit-transform: rotate(360deg);
+            }
+        }
+
+        @-webkit-keyframes reverse {
+            from {
+                -webkit-transform: rotate(360deg);
+            }
+            to {
+                -webkit-transform: rotate(0deg);
+            }
+        }
+
+        .rotate {
+            -webkit-animation-name:             rotate;
+            -webkit-animation-duration:         8s;
+            -webkit-animation-iteration-count:  infinite;
+            -webkit-animation-timing-function: linear;
+        }
+
+
+        .rotate {
+            width: 200px;
+            height:200px;
+            margin: 64px auto;
+
+        }
+
+        .vert-container {
+            height: 300px;
+            position: relative;
+        }
+
+        .vertical-center {
+            margin: 0;
+            position: absolute;
+            top: 50%;
+            -ms-transform: translateY(-50%);
+            transform: translateY(-50%);
+        }
+
+    </style>
+   <?php include_once 'views/partials/google-analytics.php'; ?>
+</head>
+<body id="birds">
+
+<div class="container pt-4 mb-4">
+    <div class="row">
+        <div id="title" class="col-12 text-center">
+
+        </div>
+
+    </div>
+    <div class="row mt-3 mb-5">
+        <div class="col-6 text-center">
+            <div class="vert-container">
+                <div class="vertical-center">
+                    <h1><a class="title-link" href="our-mission.php">simplify, automate and improve your crypto investment strategies</a></h1>
+                    <!--<h2>We simplify the process for investing in crypto currencies by carefully aligning your investment
+                    experience & strategy with mathematically validated, autonomous algorithms(bots).</h2>-->
+                </div>
+            </div>
+        </div>
+        <div class="col-6">
+            <div class="rotate">
+                <img class="center-block img-fluid" src="images/daomega-v2-icon.svg" />
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container-fluid pt-4 silver">
+
+    <div class="row mt-2">
+        <div class="col-md-3 text-center">
+            <h4>Magic Penny</h4>
+            <p><a href="compound-interest.php">a magic penny that doubles its value</a></p>
+        </div>
+        <div class="col-md-3 text-center">
+            <h4>Magic Pennies</h4>
+            <p>5 magic pennies are better then 1</p>
+        </div>
+        <div class="col-md-3 text-center">
+            <h4>Ping Pong</h4>
+            <p>two paddles and a ball</p>
+        </div>
+        <div class="col-md-3 text-center">
+            <h4>Rule of Thirds</h4>
+            <p><a href="thirds.php">33% market loss? Not a problem.</a> </p>
+        </div>
+    </div>
+    <div class="row mt-2">
+        <div class="col-md-3 text-center">
+            <h4>Leftovers</h4>
+            <p>a plate of frozen food.</p>
+        </div>
+        <div class="col-md-3 text-center">
+            <h3>Banana Split</h3>
+            <p>eat half now eat half later</p>
+        </div>
+        <div class="col-md-3 text-center">
+            <h3>Million 24</h3>
+            <p><a href="millionaire.php">one million dollars</a></p>
+        </div>
+        <div class="col-md-3 text-center">
+            <h3>Plain Jane</h3>
+            <p><a href="dollar-cost-averaging.php">dollar cost averaging</a></p>
+        </div>
+
+    </div>
+
+ </div>
+
+
+<script>
+    VANTA.BIRDS({
+        el: "#birds",
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        scaleMobile: 1.00,
+        color1: 0xff00,
+        color2: 0xff9c00,
+        speedLimit: 2.00,
+        backgroundAlpha: 0.5,
+        quantity: 2.00
+    })
+</script>
+</body>
+</html>
