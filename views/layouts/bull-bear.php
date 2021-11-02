0792f37566c1a89b1c923b45326e59ad8801431e
mikestratton <mike@mikestratton.net>
2021-11-02 01:31:39 -0400

    update

---
+++ views/layouts/bull-bear.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="we create mathematically validated crypto investment algorithms">
+    <meta name="author" content="Michael A. Stratton">
+    <!--<meta http-equiv="refresh" content="15; URL=https://daomega.com">-->
+
+    <title>crypto startup company</title>
+
+    <link rel="canonical" href="http://daomega.com">
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+
+
+    <link rel="stylesheet" href="style/cash.css">
+
+   <?php include_once 'views/partials/google-analytics.php'; ?>
+</head>
+<body>
+
+<div class="container">
+
+    <div class="row pt-2 mb-2">
+
+            <img class="center-block-2" src="images/logo-v2_website.png" />
+
+
+
+    </div>
+    <div class="row pt-2 mb-2 text-center">
+
+            <h1>Mathemagically Validated Crypto Investments</h1>
+            <p>Predict, improve & validate your returns with Daomega's crypto bots.</p>
+
+
+            <img class="center-block" src="images/bear-bull_1.png" />
+
+
+
+
+
+    </div>
+</div>
+
+
+
+</body>
+</html>
